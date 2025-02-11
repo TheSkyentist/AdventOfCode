7727c57b5fb81928a00b234bff499262d01dbfb8
TheSkyentist <raphael.hviding@gmail.com>
2025-02-11 07:37:06 +0100

    Last edits

--- 2024/src/python/__init__.py
+++ 2024/src/python/__init__.py
@@ -1,9 +1,10 @@
 # Import your day modules
-from . import day1, day2, day3, day4, day5, day6 # Add more day imports as needed
+from . import day1, day2, day3, day4, day5, day6, day7  # Add more day imports as needed
 from .download import download
 
 # Expose functions
-__all__ = ["run_day", "download"]  # Public API
+__all__ = ['run_day', 'download']  # Public API
+
 
 def run_day(day: int):
     # Match the day to the corresponding solver
@@ -20,5 +21,7 @@ def run_day(day: int):
             day5.solve()
         case 6:
             day6.solve()
+        case 7:
+            day7.solve()
         case _:
             print(f'No Python solution implemented for day {day}.')
---
+++ 2024/src/python/day7.py
@@ -0,0 +1,38 @@
+def read_input(input_str: str) -> np.ndarray:
+    return
+
+def part1():
+    return
+
+def part2():
+    return
+
+def solve():
+
+    # Define test data
+    test_data = read_input(test_data_str)
+
+    # Test
+    assert part1(test_data) == 3749
+    # assert part2(test_data) == 6
+
+    # Get input
+    input_data = read_input(download(6))
+
+    # Solve
+    print(part1(input_data))
+    # print(part2(input_data))
+
+
+
+test_data_str = """
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+"""
